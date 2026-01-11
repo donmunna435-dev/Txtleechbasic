e22c92bf0f0b623e534d71187e2ab2883c4bbbb3
donmunna435-dev <donmunna435@gmail.com>
2026-01-11 16:33:05 +0530

    Add files via upload

---
+++ vars.py
@@ -0,0 +1,25 @@
+
+
+from os import environ
+
+API_ID = i24754824on.get("API_ID", "24754824"))
+API_HASH = environ.get("API_HASH", "e24a9c7a6aa24e1c56fa349e104ec20e")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8266282399:AAHQapxFlhTrl0nd_zr5CfnOLCnqxNaBlYs")
+
+# Force Subscribe Configuration
+FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "bot_subscription")  # Channel username without @, 
+FORCE_SUB_CHANNEL_LINK = environ.get("FORCE_SUB_CHANNEL_LINK", "https://t.me/bot_subscription")  # Channel link
+
+# Admin Configuration
+ADMINS = list(map(int, environ.get("ADMINS", "").split()))
+
+# Optional: Bot Owner ID
+OWNER_ID = int(environ.get("OWNER_ID", ""))
+
+# Database URL (if you want to add database support later)
+DATABASE_URL = environ.get("DATABASE_URL", "")
+
+
+
+
+
