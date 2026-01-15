29e90c3b0c5742e6377801a61f74ff3d8cddaa7d
donmunna435-dev <donmunna435@gmail.com>
2026-01-15 20:48:12 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -3,8 +3,8 @@
 from os import environ
 
 API_ID = environ.get("API_ID", "24754824")
-API_HASH = environ.get("API_HASH", "e24a9c7a6aa24e1c56fa349e104ec20e")
-BOT_TOKEN = environ.get("BOT_TOKEN", "8266282399:AAHQapxFlhTrl0nd_zr5CfnOLCnqxNaBlYs")
+API_HASH = environ.get("API_HASH", "e24a9c7a6aa24e1c56fa349e104ee")
+BOT_TOKEN = environ.get("BOT_TOKEN", "82282399:AAHQapxFlhTrl0nd_zr5CfnOLCnqxNaYs")
 
 # Force Subscribe Configuration
 FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "bot_subscription")  # Channel username without @, 
@@ -18,6 +18,7 @@ OWNER_ID = int(environ.get("OWNER_ID", "968292174"))
 
 # Database URL (if you want to add database support later)
 DATABASE_URL = environ.get("DATABASE_URL", "")
+
 
 
 
